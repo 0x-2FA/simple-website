826d18aac4ac4729ced453cb8d021909d4daa117
0x-2FA <2fa_gh@protonmail.com>
2021-12-12 12:55:19 +0200

    Organized some files and created the views dir

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 const port = 3000;
 
+// path to all the views (html files)
+const views = '/views'
+
 // get assets from the assets directory
 app.use(express.static('assets'));
 
@@ -15,4 +18,14 @@ app.listen(port, () => {
 // render the index.html file
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+// render the about.html file
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname + views, 'about.html'));
+});
+
+// render the contact.html file
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname + views, 'contact.html'));
 });
